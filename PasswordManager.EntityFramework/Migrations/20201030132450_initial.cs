fe1a1fe62978e0cf414be6b96d23e1cdeb7ffe8e
Rinuin <the.witcher97@gmail.com>
2020-10-30 14:32:34 +0100

    Create Solution and entity framework project. Create database and manage it's connection. Create user and account entities.

---
+++ PasswordManager.EntityFramework/Migrations/20201030132450_initial.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PasswordManager.EntityFramework.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    PasswordReminder = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ PasswordManager.EntityFramework/PasswordManagerDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordManager.EntityFramework
+{
+    class PasswordManagerDbContextFactory : IDesignTimeDbContextFactory<PasswordManagerDbContext>
+    {
+        public PasswordManagerDbContext CreateDbContext(string[] args = null)
+        {
+            var options = new DbContextOptionsBuilder<PasswordManagerDbContext>();
+            options.UseSqlite("Data Source=PasswordManagerDb.db");
+            return new PasswordManagerDbContext(options.Options);
+        }
+    }
+}
